9cd937571960dd399d1011c7ddbce5746a7773c3
James Anderson <jsondev1.0@outlook.com>
2018-09-02 21:45:23 -0400

    Added guess entry

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>;
+#include <string>;
+
 using namespace std;
 
 int main() 
@@ -8,5 +10,13 @@ int main()
 	cout << "Welcome to Bulls and Cows!!" << endl;
 	cout << "Can you guess the " << WORLD_LENGTH << endl; 
 	cout << "letter isogram I'm thinking of? \n";
+
+	// get a guess from the player
+	cout << "What is your guess?? \n";
+	string Guess = "";
+	cin >> Guess;
+	cout << "You've guessed " << Guess; 
+	// repeat the guess back to them
+
 	return 0;
 }
