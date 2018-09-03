81b06e884347b8b18d2c5c67c447dd20bf302b84
James Anderson <jsondev1.0@outlook.com>
2018-09-02 21:31:58 -0400

    Testing git

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main() 
 {
+	// introduce the game
 	constexpr int WORLD_LENGTH = 3;
 	cout << "Welcome to Bulls and Cows!!" << endl;
 	cout << "Can you guess the " << WORLD_LENGTH << endl; 
