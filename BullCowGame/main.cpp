a9dc7ee28b6f2ca04f1219c3967b9dcc5662f6c9
James Anderson <jsondev1.0@outlook.com>
2018-09-02 22:32:41 -0400

    updating git ignore

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -3,21 +3,40 @@
 
 using namespace std;
 
+void PrintIntro();
+string GetGuessAndPrintBack();
+
+// entry point for the application
 int main() 
 {
-	// introduce the game
-	constexpr int WORLD_LENGTH = 3;
-	cout << "Welcome to Bulls and Cows!!" << endl;
-	cout << "Can you guess the " << WORLD_LENGTH << endl; 
-	cout << "letter isogram I'm thinking of? \n";
+	PrintIntro();
+	GetGuessAndPrintBack();
 
+	
+	
+	
+
+	return 0;
+}
+
+void PrintIntro() {
+	// introduce the game
+	constexpr int WORLD_LENGTH = 5;
+	cout << "Welcome to Bulls and Cows!!" << endl;
+	cout << "Can you guess the " << WORLD_LENGTH << endl;
+	cout << "letter isogram I'm thinking of? \n";
+	cout << endl;
+	return;
+}
+
+string GetGuessAndPrintBack() {
 	// get a guess from the player
 	cout << "What is your guess?? \n";
 	string Guess = "";
 	getline(cin, Guess);
-	
-	// repeat the guess back to them
-	cout << "You've guessed " << Guess; 
 
-	return 0;
+	// repeat the guess back to them
+	cout << "You've guessed " << Guess;
+
+	return Guess;
 }
