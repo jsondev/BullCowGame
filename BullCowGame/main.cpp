185515451a8e38468949f8914ea82fa21ca471a8
James Anderson <jsondev1.0@outlook.com>
2018-09-02 21:20:21 -0400

    init commit

---
+++ BullCowGame/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>;
+using namespace std;
+
+int main() 
+{
+	constexpr int WORLD_LENGTH = 3;
+	cout << "Welcome to Bulls and Cows!!" << endl;
+	cout << "Can you guess the " << WORLD_LENGTH << endl; 
+	cout << "letter isogram I'm thinking of? \n";
+	return 0;
+}
